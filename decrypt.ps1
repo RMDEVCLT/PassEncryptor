70dede172cdb598b22259de86b0e8aa11018871a
RMDEVCLT <116014388+RMDEVCLT@users.noreply.github.com>
2024-09-03 19:51:01 -0400

    Update decrypt.ps1

--- decrypt.ps1
+++ decrypt.ps1
@@ -1,15 +1,4 @@
-<#
-#wrote this in the event I write a CSharp version of the code.
-# Step 1: Load the DLL into the PowerShell session
-Add-Type -Path "C:\Path\To\Your\PasswordDecryptor.dll"
 
-# Step 2: Call the DecryptPassword method from PowerShell
-$decryptedPassword = [PasswordDecryptor]::DecryptPassword("C:\temp\myPassword.txt", "C:\temp\myKey.key")
-
-# Step 3: Output the decrypted password
-Write-Output $decryptedPassword
-==================================
-#>
 # Use the code below
 # Get the encrypted password
 $encryptedPassword = Get-Content "c:\temp\myPassword.txt" -Raw
